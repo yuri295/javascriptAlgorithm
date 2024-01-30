214f9d30d85250a4ba8314a36f1260d679a2dd10
yuri295 <jin_astro0302@naver.com>
2024-01-30 13:39:07 +0900

    javascript 반복문 문제

--- javascript-basic-test/합.js
+++ javascript-basic-test/합.js
@@ -2,6 +2,7 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let n = Number(input[0]);
+let sum = 0;
 
 for (let i = 1; i < n + 1; i++) {
   sum += i;
