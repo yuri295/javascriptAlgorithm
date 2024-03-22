50fd369ecff1d31521d10ace7ca3dddd52411b32
yuri295 <jin_astro0302@naver.com>
2024-03-22 15:23:37 +0900

    정렬 알고리즘 문제

---
+++ javascript-sort/나이순 정렬.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let N = Number(input[0]);
+let arr = [];
+
+for (let i = 1; i <= N; i++) {
+  //각 사람의 (나이, 이름) 정보를 입력받기
+  let age = Number(input[i].split(' ')[0]);
+  let name = input[i].split(' ')[1];
+  arr.push([age, name]);
+}
+
+arr.sort((a, b) => a[0] - b[0]); // Node.js의 정렬은 기본적으로 stable
+
+let answer = '';
+for (let x of arr) answer += x[0] + ' ' + x[1] + '\n';
+console.log(answer);
---
+++ javascript-sort/단어 정렬.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(input[i]);
+}
+
+arr = [...new Set(arr)];
+
+arr.sort((a, b) => {
+  if (a.length != b.length) return a.length - b.length;
+  else {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
+});
+
+for (let x of arr) {
+  console.log(x);
+}
